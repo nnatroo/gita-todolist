f7a195ede909b2934a0aac4ba970c34edbec644e
superrwholockk <teogotsiridze@yahoo.com>
2026-02-19 19:58:26 +0400

    Complete renderTodos functionality

--- script.js
+++ script.js
@@ -2,17 +2,27 @@ const todoForm = document.getElementById('todo-form');
 const todoInput = document.getElementById('todo-input');
 const todoList = document.getElementById('todo-list');
 
-const todos = [];
+const todos = JSON.parse(localStorage.getItem("todos")) || [];
 
 function saveToLocalStorage() {
     localStorage.setItem('todos', JSON.stringify(todos));
+}
+
+function renderTodos() {
+    todoList.innerHTML = "";
+    todos.forEach(todo => {
+        let li = document.createElement('li');
+        li.textContent = todo.text;
+        todoList.appendChild(li);
+
+    })
 }
 
 todoForm.addEventListener('submit', (e) => {
     e.preventDefault();
 
     const inputValue = todoInput.value.trim();
-    console.log(inputValue);
+    if (inputValue === "")return;
 
     const newTodo = {
         id: Date.now(),
@@ -21,4 +31,9 @@ todoForm.addEventListener('submit', (e) => {
     }
     todos.push(newTodo);
     saveToLocalStorage();
+    renderTodos();
+    todoInput.value = "";
 })
+
+
+
